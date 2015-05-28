eec32fd431d285132a21f7871efa65c20eef9edf
dooqaqa <dooqaqa@sina.cn>
2015-05-28 21:57:45 +0800

    更新文章读取代码

--- htdocs/load_blog.php
+++ htdocs/load_blog.php
@@ -1,5 +1,6 @@
 
 <?php
+echo "00000";
 require("key.php");
 define(DB_TABLENAME, 'blogs');
 define(DB_USERNAME, 'user');
@@ -9,29 +10,47 @@ $time=$_POST["qq"];
 $sql="";
 $blogs_per_time = 5;
 $file_found = 0;
+echo "111111";
 
-$dblink = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("connect failed" . mysql_error());  
+$dblink = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("connect failed" . mysql_error());
+echo "222222";
 if (!$dblink) {
+echo "3333333";
 	die('Could not connect: ' . mysql_error());
 }
+echo "4444444";
 mysql_select_db(DB_DATABASENAME, $dblink);
-if ("" == $time) {	
+echo "55555";
+if (null == $time or "" == $time) {
+echo "66666";	
 	$sql = sprintf("select * from %s order by post_time limit %d", DB_TABLENAME, $time, $blogs_per_time);
 } else {
+echo "777777";	
 	$sql = sprintf("select * from %s where post_time < '%s' order by post_time limit %d", DB_TABLENAME, $time, $blogs_per_time);
 }
+echo "888888";	
 $result = mysql_query($sql);
+echo "99999";	
 
 while($row = mysql_fetch_array($result) and $file_found < $blogs_per_time) {
-	echo $row['FirstName'];
-	echo " " . $row['LastName'];
-	echo " " . $row['Age'];
-	echo "<br />";
-	if ($fp = fopen("blogs/" . $topic . ".txt", "w")) {
-		if (fwrite($fp, $body)) {
+	if ($fp = fopen("blogs/" . $row['file_name'], "r")) {
+		$body = "";
+		if (fread($fp, $body)) {
 			fclose($fp);
-			save_blog_db($topic);
-			echo $topic . "good";
+			echo "<blog>";
+			echo "<topic>";
+			echo $row['topic'];
+			echo "</topic>";
+			echo "<time>";
+			echo $row['post_time'];
+			echo "</time>";
+			echo "<cat>";
+			echo $row['category'];
+			echo "</cat>";
+			echo "<body>";
+			echo $body;
+			echo "</body>";
+			echo "</blog>";
 		} else {
 			echo "bad2";
 			fclose($fp);
